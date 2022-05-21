24029fd4ed4e2ca658eb5a21fb8b1dc12697cffc
arshiha bristy <arshihabristy5@gmail.com>
2022-05-22 00:12:04 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        bicycletheme: {
+          primary: "#00FFFF",
+          secondary: "#7FFFD4",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
